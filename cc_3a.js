ff0d287536842f3d75e04b6b9338a50429bc45d3
Tristan Zybala <tzybala@usf.edu>
2025-09-14 10:27:51 -0400

    Create Customer Records

---
+++ cc_3a.js
@@ -0,0 +1,19 @@
+// Coding Challenge 3a
+
+// Create Customer Records
+let customers = [
+    {name:"Michael", email:"michael@email.com", purchases:["Bread","Cheese","Ham"]},
+    {name:"William", email:"william@email.com", purchases:["Butter","Toilet Paper","Ground Beef"]},
+    {name:"David", email:"david@email.com", purchases:["Pasta","Cookies","Apple"]},
+];
+
+console.log(customers);
+
+// Add & Remove Data
+
+
+// Update Customer Info
+
+
+// Display Customer Information
+
