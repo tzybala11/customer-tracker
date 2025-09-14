998ead341b7aa1c4b088a2fc7f035446ed7acc14
Tristan Zybala <tzybala@usf.edu>
2025-09-14 10:52:26 -0400

    Add & Remove Data

--- cc_3a.js
+++ cc_3a.js
@@ -7,10 +7,16 @@ let customers = [
     {name:"David", email:"david@email.com", purchases:["Pasta","Cookies","Apple"]},
 ];
 
-console.log(customers);
+    console.log(customers);
 
 // Add & Remove Data
+customers.push({name:"Lisa", email:"lisa@email.com", purchases:["Shirt","Shoes","Doritos"]});
 
+    console.log(customers);
+
+customers.shift();
+
+    console.log(customers);
 
 // Update Customer Info
 
